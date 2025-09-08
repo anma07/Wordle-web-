22bbeb8217c7d92697e37a34e9e0c581240e050e
Avani Mandlik <avanimandlik27@gmail.com>
2025-09-08 12:24:22 +0530

    Update 2

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-var word;
+var word = "Avani";
 var COL = 5;
 var ROW = 6;
 var currentCol = 0;
 var currentRow = 0;
 const cells = document.querySelectorAll("td");
 
+function currentBox(row, col){
+    return row * COL + col;
+  }
+
 document.addEventListener('keydown',function(event){
 
   let key = event.key.toUpperCase(); 
-
-  function currentBox(row, col){
-    return row * COL + col;
-  }
   
   if (/^[A-Z]$/.test(key) && currentCol < COL) {
     let index = currentBox(currentRow, currentCol);
@@ -28,6 +28,7 @@ document.addEventListener('keydown',function(event){
         currentCol = 0;
     }
     cells[index].textContent = "";
+
   }
 
   if (event.key === "Delete" && currentCol > 0){
@@ -38,16 +39,38 @@ document.addEventListener('keydown',function(event){
         cells[index].textContent = "";
     }
     currentCol = 0;
+  }
 
-if (event.key === "Enter" && currentCol === COL){
-    currentRow++;
+  if (event.key === "Enter"){
+    if(currentCol === 5){
+      for(i=0; i<5; i++){
+        let index = currentBox(currentRow, i);
+        let cell = cells(index);
+        let letter = cell.textContent;
+        if (letter === word[i]){
+          cell.classList.add("correct");
+        }
+      }
+      for(i=0; i<5; i++){
+        for(j=0; j<5; j++){
+          let index = currentBox(currentRow, i);
+          let cell = cells(index);
+          let letter = cell.textContent;
+          if(letter === word[j]){
+            cell.classList.add("present");
+            break;
+          }
+          let index = currentBox(currentRow, i);
+          let cell = cells(index);
+          cell.classList.add("absent");
+        }
+      }
+      currentRow ++;
+      currentCol = 0;
+  }
+  else{
+    alert("Pls Enter The full word!")
+  }
+  console.log(currentCol);
 }
-
-if (event.key === "Enter" && currentCol !== COL){
-    alert("Pls Enter full Word");
-}
-// while(currentRow != 6){
-//     let index = currentBox(currentRow, currentCol);
-//     if()
-// }
-}});
+});
